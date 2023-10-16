c809d7ed3d8344b1f7853fde67b286711f84b768
ItsMatDev <thierry.matthieu@live.fr>
2023-10-16 17:28:09 +0200

    adding welcome message

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import Title from "./components/Title/Title";
 import Presentation from "./components/Presentation/Presentation";
-import Like from "./components/Like/Like";
-import Install from "./components/Install/Install";
 import Playground from "./components/Playground/Playground";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [isDivVisible, setIsDivVisible] = useState(false);
+  const [display, setDisplay] = useState("block");
+  const [zIndex, setZindex] = useState(1);
+
+  useEffect(() => {
+    return setIsDivVisible(true);
+  }, []);
   return (
     <>
+      <div className="welcomeClick" onClick={() => setDisplay("none")} onClickCapture={() => setZindex(-1)}>
+        <div className="welcomeBox" style={{ display: display, zIndex: zIndex }}>
+          {isDivVisible && <p className="welcomeText">Bienvenu sur ce magnique site fan de React & Vite</p>}
+        </div>
+      </div>
       <Title />
       <main>
         <Presentation />
