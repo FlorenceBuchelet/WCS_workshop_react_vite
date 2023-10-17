e9d6e32ce183fcfc8f8a8045e5f2331042d74374
ItsMatDev <thierry.matthieu@live.fr>
2023-10-17 10:28:17 +0200

    moved some fils and create WelcomeMessage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,16 @@
 import Title from "./components/Title/Title";
 import Presentation from "./components/Presentation/Presentation";
 import Playground from "./components/Playground/Playground";
+import WelcomeMessage from "./components/WelcomeMessage/WelcomeMessage";
 import { useState } from "react";
 import "./App.css";
 
 function App() {
   const [display, setDisplay] = useState("block");
-  const [zIndex, setZindex] = useState(1);
 
-  
   return (
     <>
-      <div className="welcomeClick" onClick={() => {setDisplay("none"); setZindex(-1)}} style={{ display: display }}>
-        <div className="welcomeBox" style={{ display: display, zIndex: zIndex }}>
-         <p className="welcomeText">Bienvenue sur ce magnifique fansite dédié à React & Vite !</p>
-        </div>
-      </div>
+      <WelcomeMessage display={display} setDisplay={setDisplay} />
       <Title />
       <main>
         <Presentation />
